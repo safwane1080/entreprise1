bf3a5231bf73d04850be41de4006a78ef4bd8238
safwane1080 <safwane.el.masaoudi@student.ehb.be>
2025-12-25 14:27:04 +0100

    toevoegen van users

--- src/main/java/be/entreprise/entreprise1/model/User.java
+++ src/main/java/be/entreprise/entreprise1/model/User.java
@@ -1,13 +1,46 @@
 package be.entreprise.entreprise1.model;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
     private String email;
+
+    private String password;
+
+    private String role;
+
+    public Long getId() {
+        return id;
+    }
 
     public String getEmail() {
         return email;
     }
 
+    public String getPassword() {
+        return password;
+    }
+
+    public String getRole() {
+        return role;
+    }
     public void setEmail(String email) {
         this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
     }
 }
